fdec5c795c88314ecd13dc7aa6d94156d1bec0d4
Emanuel Handoyo <emanuelhandoyo@outlook.com>
2021-11-25 22:27:18 +0700

    First time Commit
    Init version. for manual testing, use native.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<form action="denomination.php" method="post">
+Your money: <input type="text" name="money"><br><input type="submit">
+</form>
+
+</body>
+</html>
---
+++ native.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+
+$input = 29387492;
+
+function denomination($amt){
+    $paper = [100000 => 0, 50000 => 0, 20000 => 0, 10000 => 0, 5000 => 0, 2000 => 0, 1000 => 0];
+    $coin = [500 => 0, 200 => 0, 100 => 0];
+    $str= "Uang anda: Rp ".number_format($amt,2,',','.')."<hr><br>";
+    krsort($paper);// if given array isn't sorted in descending order
+    foreach($paper as $money_type => $qty){
+        $qty = intval($amt / $money_type);
+        $amt -= $money_type * $qty;
+        $paper[$money_type] = $qty;
+        $str.= $qty."x lembar Rp ".number_format($money_type,2,',','.')." <br>";
+        
+
+    }
+    
+    foreach($coin as $money_type => $qty){
+        $qty = intval($amt / $money_type);
+        $amt -= $money_type * $qty;
+        $coin[$money_type] = $qty;
+        $str.= $qty."x keping Rp ".number_format($money_type,2,',','.')." <br>";
+    }
+    
+    $str.= "<hr>Sisa: Rp ".number_format($amt,2,',','.');
+    
+    
+    
+    return $str;
+}
+
+echo denomination($input);
+?>
+
+</body>
+</html>
